6c2a6fd8321b8a08f4213c53cc3e47a2d6e40e6f
hrathnapala <43751413+hrathnapala@users.noreply.github.com>
2020-07-21 07:44:57 +0530

    initial

---
+++ app/src/main/java/com/example/testweapon1/SplashActivity.java
@@ -0,0 +1,32 @@
+package com.example.testweapon1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    Handler handler;
+    Runnable runnable;
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        imageView = findViewById(R.id.imageView);
+        imageView.animate().alpha(4000).setDuration(0);
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        },4000);
+
+    }
+}
